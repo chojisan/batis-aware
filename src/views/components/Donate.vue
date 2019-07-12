7cab1da0036b4529bfcc67ab091f19df7831b87c
Seigi Takada <seiji.takada@medixserve.com>
2019-07-12 14:42:02 +0800

    Added donate

---
+++ src/views/components/Donate.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id="donate">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <div class="content-donate">
+            <div class="donate-left">HELP US FUND OUR ADVOCACIES</div>
+            <div class="donate-right">
+              <a href="#">DONATE NOW!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
