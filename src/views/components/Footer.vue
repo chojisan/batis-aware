37d57bef890a9088bba57019a61fb02649c88c85
Seigi Takada <seiji.takada@medixserve.com>
2019-07-12 14:43:12 +0800

    Added footer

---
+++ src/views/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-lg-left text-center">
+          <div class="copyright">
+            &copy; 2018 Copyright
+            <strong>Batis AWARE - Association of Women in Action for Rights and Empowerment</strong>.
+            All Rights Reserved
+          </div>
+          <div class="credits">
+            Powered by
+            <a href="https://takada.tech">Takada Teknolohiya</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
