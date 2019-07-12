eaa2c8fd4dd528e854dc0fb08f32aec81fe8f386
Seigi Takada <seiji.takada@medixserve.com>
2019-07-12 14:41:43 +0800

    Added activities

---
+++ src/views/components/Activities.vue
@@ -0,0 +1,15 @@
+<template>
+  <section id="activities">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 text-center" data-aos="zoom-in">
+          <h2 class="section-heading text-uppercase">OUR ACTIVITIES</h2>
+          <h3 class="section-subheading text-muted">sub header</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-3"></div>
+      </div>
+    </div>
+  </section>
+</template>
