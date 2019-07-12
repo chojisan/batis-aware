1ceccd2f65d1356b83344a3e6af6f5555da47a1c
Seigi Takada <seiji.takada@medixserve.com>
2019-07-12 14:43:00 +0800

    Added what we do

---
+++ src/views/components/WhatWeDo.vue
@@ -0,0 +1,139 @@
+<template>
+  <section id="what-we-do">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 text-center" data-aos="zoom-in">
+          <h2 class="section-heading text-uppercase">What We Do</h2>
+          <h3 class="section-subheading text-muted">Women Empowerment and Advocacies</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0 advocacy-items"
+          data-aos="fade-up"
+          data-aos-delay="150"
+        >
+          <div class="icon-box active">
+            <figure class="d-flex justify-content-center">
+              <img src="@/assets/img/what-we-do/hands-gray.png" alt>
+              <img src="@/assets/img/what-we-do/hands-white.png" alt>
+            </figure>
+
+            <header class="entry-header">
+              <h3 class="entry-title">Become a Volunteer</h3>
+            </header>
+
+            <div class="entry-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris quis aliquam.</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0 advocacy-items"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <div class="icon-box">
+            <figure class="d-flex justify-content-center">
+              <img src="@/assets/img/what-we-do/teamwork-gray.png" alt>
+              <img src="@/assets/img/what-we-do/teamwork-white.png" alt>
+            </figure>
+
+            <header class="entry-header">
+              <h3 class="entry-title">Peer Counseling</h3>
+            </header>
+
+            <div class="entry-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris quis aliquam.</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0 advocacy-items"
+          data-aos="fade-up"
+          data-aos-delay="450"
+        >
+          <div class="icon-box">
+            <figure class="d-flex justify-content-center">
+              <img src="@/assets/img/what-we-do/hands-gray.png" alt>
+              <img src="@/assets/img/what-we-do/hands-white.png" alt>
+            </figure>
+
+            <header class="entry-header">
+              <h3 class="entry-title">Theater Advocacy</h3>
+            </header>
+
+            <div class="entry-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris quis aliquam.</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0 advocacy-items"
+          data-aos="fade-up"
+          data-aos-delay="150"
+        >
+          <div class="icon-box">
+            <figure class="d-flex justify-content-center">
+              <img src="@/assets/img/what-we-do/charity-gray.png" alt>
+              <img src="@/assets/img/what-we-do/charity-white.png" alt>
+            </figure>
+
+            <header class="entry-header">
+              <h3 class="entry-title">Business Enterprise</h3>
+            </header>
+
+            <div class="entry-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris quis aliquam.</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0 advocacy-items"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <div class="icon-box">
+            <figure class="d-flex justify-content-center">
+              <img src="@/assets/img/what-we-do/hands-gray.png" alt>
+              <img src="@/assets/img/what-we-do/hands-white.png" alt>
+            </figure>
+
+            <header class="entry-header">
+              <h3 class="entry-title">Membership and Organizing</h3>
+            </header>
+
+            <div class="entry-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris quis aliquam.</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0 advocacy-items"
+          data-aos="fade-up"
+          data-aos-delay="450"
+        >
+          <div class="icon-box">
+            <figure class="d-flex justify-content-center">
+              <img src="@/assets/img/what-we-do/hands-gray.png" alt>
+              <img src="@/assets/img/what-we-do/hands-white.png" alt>
+            </figure>
+
+            <header class="entry-header">
+              <h3 class="entry-title">Education and Training</h3>
+            </header>
+
+            <div class="entry-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris quis aliquam.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
